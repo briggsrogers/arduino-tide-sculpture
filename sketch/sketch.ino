a3a2c86b7d577863f1f6a81a19ce01f40364bd70
Brian Rogers <rogbrian@amazon.com>
2020-11-29 00:02:33 +0000

    logging and other web stuff

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,41 +1,92 @@
-// BR Edit 
+
+// Web Dependencies 
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <ArduinoJson.h>
+
+
+// Internet
+// ********************************
+  byte mac[] = { 0xAE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; 
+
+  // Set the static IP address to use if the DHCP fails to assign
+  IPAddress ip(192, 168, 0, 177);
+  IPAddress myDns(192, 168, 0, 1);
+// ********************************
+
+//  Time Dependencies 
+// ********************************
+
+long recievedTime;
+unsigned int localPort = 8888;
+const char timeServer[] = "time.nist.gov";
+const int NTP_PACKET_SIZE = 48;
+byte packetBuffer[NTP_PACKET_SIZE];
+
+EthernetUDP Udp;
+
+long initialMillis;
+
+// ********************************
 
 // Pin Assignments
-int outputPins[] = {3, 5, 6, 9, 10, 11};
+int outputPins[] = {2, 3, 5, 6, 7, 8}; // UNO
+//int outputPins[] = {0, 1, 2, 3, 4, 5};
 float numOfChannels = 6;
-float lengthOfTideCycle = 2000; // Tide cycle in seconds (44640). Shorten for debug
+int lengthOfTideCycle = 22350; // Tide cycle L->H in seconds (22350). Shorten for debug
 
-// Time Remaining (Mins until high tide. Min: 0, max: 744.)
-float t = 300;
-float interval = 1000; // One second
+// Tide Logic Variables
+// ********************************
+// t: seconds elapsed in current cycle
+float t = 200;
+
+// Tick rate (one second)
+float interval = 1000; 
 
 // Direction
 float direction = 1; // 1 = rising | -1 = falling
 
+// Last low tide (Nov 27 @ 10:03am)
+// Updated by getLastLowTide()
+long lastlow = 1606471380;
+// ********************************
+
 // Setup
 void setup() {
   Serial.begin(9600);
-  Serial.println ("Initializing...");
-  // Set PinMode
+
+  // Set PinModes
   setPinModes();
+
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
+
+  Serial.println("Initializing...");
+  getTimeFromNTP();
+  Serial.print("Recieved current time: ");
+  Serial.println(recievedTime);
+
+  calibrate();
 }
 
 void loop() {
+  
+  int timeElapsed = millis() / 1000;
 
-  // Raise and lower t value
-  setTidePosition();
+  // Adjust tide ever 10 seconds
+  if(timeElapsed % 10 == 0){
+    // Raise and lower t value
+    setTidePosition();
+    lightPins();
+  }
+  
+  // Adjust tide ever 30 seconds
+  if(timeElapsed % 30 == 0){
+    log();
+  }
 
-  // Set pin values
-  lightPins();
-
-  Serial.println (" | ");
-  Serial.print ("Millis: ");
-  Serial.print (millis() / 1000);
-  Serial.print (" | ");
-  Serial.print ((t/lengthOfTideCycle) * 100);
-  Serial.print ("% | ");
-
-  delay(interval);
 }
 
 void setTidePosition(){          
@@ -53,7 +104,7 @@ void setTidePosition(){
    }
 
    // Set t
-   float tVal = t + (1 * direction); // Increment or decrement by 1;
+   float tVal = t + (10 * direction); // Increment or decrement by 1;
    t = tVal;
 }
 
@@ -100,7 +151,7 @@ void lightPins(){
           Serial.print (" | Channel: ");
           Serial.print (channelNumber);
           Serial.print (" | Value: ");
-          Serial.print (value);
+          Serial.println (value);
         }
      }
   }
@@ -114,3 +165,192 @@ void setPinModes() {
     pinMode(pin, OUTPUT);
   }
 }
+
+void calibrate(){
+  // Send HTTP request
+  if (!Ethernet.begin(mac)) {
+    Serial.println(F("Failed to configure Ethernet"));
+    return;
+  }
+  delay(1000);
+  // Connect to HTTP server
+  EthernetClient client;
+  client.setTimeout(10000);
+  if (!client.connect("dweet.io", 80)) {
+    Serial.println(F("Connection failed"));
+    return;
+  }
+
+  Serial.println(F("Connected!"));
+  client.println(F("GET /get/latest/dweet/for/tidelight-v1 HTTP/1.1"));
+  client.println(F("Host: dweet.io"));
+  client.println(F("Connection: close"));
+  if (client.println() == 0) {
+    Serial.println(F("Failed to send request"));
+    return;
+  }
+
+  // Check HTTP status
+  char status[32] = {0};
+  client.readBytesUntil('\r', status, sizeof(status));
+  if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+    Serial.print(F("Unexpected response: "));
+    Serial.println(status);
+    return;
+  }
+
+  // Skip HTTP headers
+  char endOfHeaders[] = "\r\n\r\n";
+  if (!client.find(endOfHeaders)) {
+    Serial.println(F("Invalid response"));
+    return;
+  }
+
+  // Allocate the JSON document
+  // Use arduinojson.org/v6/assistant to compute the capacity.
+  const size_t capacity = 210;
+  DynamicJsonDocument doc(capacity);
+
+  // Parse JSON object
+  DeserializationError error = deserializeJson(doc, client);
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  // Extract values
+  Serial.println(F("Response:"));
+  Serial.println((doc["with"][0]["content"]["lastlow"].as<long>()));
+
+  lastlow = doc["with"][0]["content"]["lastlow"];
+
+  // current time as recived from NTC
+  int timeSinceLastLow = recievedTime - lastlow;
+  int remainder = timeSinceLastLow % lengthOfTideCycle;
+  t = remainder;
+
+  // Disconnect
+  client.stop();
+}
+
+// Request time from Network Time Protocol
+// https://www.arduino.cc/en/Tutorial/LibraryExamples/UdpNtpClient
+void getTimeFromNTP(){
+
+  Serial.println(F("Getting time from NTP..."));
+
+  // start Ethernet and UDP
+  if (Ethernet.begin(mac) == 0) {
+      Serial.println("Failed to configure Ethernet using DHCP");
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      
+    } else if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    while (true) {
+      Serial.println("Abandoning..");
+      return;
+    }
+  }
+
+  Udp.begin(localPort);
+
+  delay(500);
+  sendNTPpacket(timeServer); 
+  delay(500);
+
+  if (Udp.parsePacket()) {
+    // We've received a packet, read the data from it
+    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    // the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, extract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+   
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+    recievedTime = epoch;
+
+    Udp.stop();
+  }
+  else{
+    Serial.println(F("Trying NTC server again..."));
+    Ethernet.maintain();
+    delay(500);
+    getTimeFromNTP();
+  }
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(const char * address) {
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); // NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+
+void log(){
+  Serial.println(F("Logging..."));
+
+  // Send HTTP request
+  if (!Ethernet.begin(mac)) {
+    Serial.println(F("Failed to configure Ethernet"));
+    return;
+  }
+  delay(1000);
+  // Connect to HTTP server
+  EthernetClient client;
+  client.setTimeout(10000);
+
+  if (!client.connect("dweet.io", 80)) {
+    Serial.println(F("Connection failed"));
+    return;
+  }
+
+  // Prepare JSON document
+  DynamicJsonDocument doc(255);
+  doc["position"] = (t / lengthOfTideCycle) * 100;
+
+  Serial.println(F("Connected!"));
+  client.println(F("POST /dweet/for/arduino-tide-metrics-v1 HTTP/1.1"));
+  client.println(F("Host: dweet.io:443"));
+  client.println(F("Content-Type: application/json"));
+  client.println(F("Connection: close"));
+  client.print(F("Content-Length: "));
+  client.println(measureJsonPretty(doc));
+  client.println();
+  
+  serializeJsonPretty(doc, client);
+
+  // Disconnect
+  client.stop();
+
+}
+
